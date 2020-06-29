3f039912b56190abca02c17ac589f0e358cdf679
Essam <eemorsi@gmail.com>
2020-06-29 13:45:19 +0200

    correct the learner python file

--- coupling/CSM.py
+++ coupling/CSM.py
@@ -2,7 +2,7 @@ from flowvrapp import *
 from pdi_flowvr import Module_PDI
 
 envmodule = Module_PDI("env", cmdline = "./run_dssat C UFGA8201.MZX 1", pdi_conf = "env.yml")
-rlmodule = Module_PDI("learner", cmdline = "python getter.py", pdi_conf = "learner.yml")
+rlmodule = Module_PDI("learner", cmdline = "python rl_learner.py", pdi_conf = "learner.yml")
 
 envmodule.getPort("text").link(rlmodule.getPort("text"))
 rlmodule.getPort("rl_val").link(envmodule.getPort("rl_val"))
