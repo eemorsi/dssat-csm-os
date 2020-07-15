1b1d82cfecd58df38afe83729ebd4f9d7f986a3e
Essam <eemorsi@gmail.com>
2020-07-15 07:28:37 +0200

    remove rlval port from the config - not working yet

--- PyTemp/PYTEST.py
+++ PyTemp/PYTEST.py
@@ -2,9 +2,9 @@ from flowvrapp import *
 from pdi_flowvr import Module_PDI
 
 envmodule = Module_PDI("env", cmdline = "./cputter", pdi_conf = "env.yml")
-rlmodule = Module_PDI("learner", cmdline = "python rl_learner.py", pdi_conf = "learner.yml")
+rlmodule = Module_PDI("learner", cmdline = "python learner.py", pdi_conf = "learner.yml")
 
 envmodule.getPort("text").link(rlmodule.getPort("text"))
-rlmodule.getPort("rlval").link(envmodule.getPort("rlval"))
+# rlmodule.getPort("rlval").link(envmodule.getPort("rlval"))
       
 app.generate_xml("PYTEST")
--- PyTemp/putter.c
+++ PyTemp/putter.c
@@ -1,18 +1,26 @@
+/**
+ * gcc -O2 putter.c -o cputter -lpdi -lparaconf
+ * */
+
 #include <stdio.h>
 #include <pdi.h>
 int main(int argc, char const *argv[])
 {
-    PC_tree_t node = PC_parse_path("put.yml");
+    PC_tree_t node = PC_parse_path("env.yml");
     PDI_init(PC_get(node, ".pdi"));
 
-    int RUN=1;
+    int RUN = 1;
     int HASN = 0;
     PDI_expose("RUN_SE", &RUN, PDI_IN);
-    while (RUN) {
+    while (RUN)
+    {
         PDI_expose("HASN", &HASN, PDI_OUT);
         printf("C HASN: %d\n", HASN);
 
-        PDI_expose("RUN_SE", &RUN , PDI_IN);
+        PDI_expose("RUN_SE", &RUN, PDI_IN);
+        
+
+        RUN = HASN < 20 ? RUN : 0;
         HASN++;
     }
 
